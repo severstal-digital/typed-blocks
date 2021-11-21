8b3ef31fb0401d9814512cbf3c9f49f9a37e4d5a
tribunsky.kir <tribunsky.kir@gmail.com>
2021-11-21 23:05:55 +0300

    feat: backport of redis streams almost as-is

---
+++ blocks/redis/processors.py
@@ -0,0 +1,54 @@
+from typing import Dict, List, Type, Union
+
+from redis import Redis
+
+from blocks import Event, Processor
+from blocks.redis.serdes import Serializer, serialize
+from blocks.redis.streams import OutputStream
+
+
+class RedisProducer(Processor):
+    """
+    Class represents event processor that writes messages to Redis streams
+    based on data received from event. On initialization must get list
+    of output streams in order to perform arbitrary mapping. In most cases
+    you don't need to use RedisProducer directly, RedisApp can make this for you.
+
+    Example::
+
+      >>> from redis import Redis
+      >>> from blocks import Event, Graph
+      >>> from blocks.redis import RedisProducer, OutputStream
+      >>>
+      >>> class MyEvent(Event):
+      ...     x: int
+      >>>
+      >>> streams = [OutputStream('some_stream', MyEvent)]
+      >>> graph = Graph()
+      >>> client = Redis(...)
+      >>> graph.add_block(RedisProducer(client, streams))
+    """
+
+    def __init__(
+        self,
+        client: Redis,
+        streams: List[OutputStream],
+        serializer: Serializer = serialize
+    ) -> None:
+        self._client = client
+        self._serializer = serializer
+
+        self._streams: Dict[Type[Event], OutputStream] = {stream.event: stream for stream in streams}
+        self.__call__.__annotations__.update(
+            {
+                'event': Union[tuple(stream.event for stream in streams)],
+            },
+        )
+
+    def __call__(self, event: Event) -> None:
+        stream = self._streams[type(event)]
+        serialized = self._serializer(event.dict())
+        self._client.xadd(stream.name, fields=serialized, maxlen=stream.max_len)  # type: ignore
+
+    def close(self) -> None:
+        ...
